643061a90f881a7d40041b75ae30d62bb45e3ece
6fy <97406109+6fy@users.noreply.github.com>
2022-08-20 11:31:42 +0200

    cleaning up
    - no error on title because i fetch the title from the downloaded file
    - more options to change within the code (may update later for non coders)
    - more exceptions handlers

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ try:
     import re
     import urllib.request
     import time
+    import string
 except (ImportError or ModuleNotFoundError) as importError:
     print("""
     You need to install all the modules
@@ -26,6 +27,13 @@ except (ImportError or ModuleNotFoundError) as importError:
     """)
     input('Press enter to close this window.')
     sys.exit(1)
+
+AUTHOR = "https://github.com/6fy"
+DOWNLOAD_PATH = "downloads/"
+MAX_RESULTS = 5
+
+QUIT_CHARS = ["quit", "q", "cancel", "none", "exit", "close"]
+VALID_CHARS = list(string.ascii_lowercase + " " + string.digits)
 
 class Downloader:
     def __init__(self):
@@ -74,7 +82,7 @@ class Downloader:
         This function checks if the path exists if it doesn't then creates the path\n
         folder = the folder name
         """
-        path = os.getcwd() + '/downloads/' + folder
+        path = os.getcwd() + "/" + DOWNLOAD_PATH + folder
         if not os.path.exists(path):
             os.makedirs(path)
 
@@ -82,20 +90,20 @@ class Downloader:
 
 
     # ========================= >
-    # Print video details
+    # log video details
     # < =========================
-    def print_video_details(self, yt):
+    def log_video_details(self, yt):
         """
-        This function prints details of the video\n
+        This function logs details of the video\n
         yt = youtube object
         """
         minutes = yt.length / 60
-        print('\n---------------------------------------')
-        print(f"Title: {yt.title}")
-        print(f'Url: https://www.youtube.com/watch?v={yt.video_id}')
-        print(f"Number of views: {yt.views}")
-        print(f"Length of video: {round(minutes, 2)} minutes ({yt.length} seconds)")
-        print('---------------------------------------\n')
+        log('\n---------------------------------------')
+        log(f"Title: {yt.title}")
+        log(f'Url: https://www.youtube.com/watch?v={yt.video_id}')
+        log(f"Number of views: {yt.views}")
+        log(f"Length of video: {round(minutes, 2)} minutes ({yt.length} seconds)")
+        log('---------------------------------------\n')
 
 
     # ========================= >
@@ -110,32 +118,34 @@ class Downloader:
         ys = yt.streams.get_highest_resolution()
 
         #Starting download
-        print(f'Downloading "{yt.title}"...')
-        print(f"Details: {ys}")
+        log(f'Downloading "{yt.title}"...')
+        log(f"Details: {ys}")
 
         path = self.new_video_folder(currentTime)
-        ys.download(path)
+        file_name = ys.download(path)
+
+        return file_name
 
 
     # ========================= >
     # Converting the video to mp3
     # < =========================
-    def convert_to_sound(self, yt, currentTime):
+    def convert_to_sound(self, currentTime, title):
         """
         This function converts the video to mp3\n
         yt = YouTube object\n
         path = path to save the mp3 file\n
         """
         try:
-            path = path = self.new_video_folder(currentTime)
-            title = yt.title.replace('.', '').replace(',', '').replace('*', '') \
-                .replace('/', '').replace('\\', '').replace(':', '').replace('?', '') \
-                .replace('<', '').replace('>', '').replace('|', '').replace(':', '')
-            video = VideoFileClip(os.path.join(path, title + ".mp4"))
+            path = self.new_video_folder(currentTime)
+
+            video = VideoFileClip(os.path.join(path, title))
             video.audio.write_audiofile(os.path.join(path, title + ".mp3"))
         except Exception as e:
-            print(e)
-            print("Error: An exception occurred trying to write to mp3.")
+            log(e)
+            log("Error: An exception occurred trying to write to mp3.")
+            return False
+        return True
 
 
     # ========================= >
@@ -146,7 +156,7 @@ class Downloader:
         This function get the youtube title from the link if it wasn't a link but text\n
         title = the title of the video aka the link
         """
-        print('looking at youtube...')
+        log('looking at youtube...')
 
         # Send a request with the text
         search_keyword = title.replace(' ', '+')
@@ -161,31 +171,31 @@ class Downloader:
 
 
     # ========================= >
-    # Print the available results
+    # log the available results
     # < =========================
-    def print_results(self, results):
-        """
-        This function prints 5 results from youtube and returns them in an array\n
+    def log_results(self, results):
+        f"""
+        This function logs {MAX_RESULTS} results from youtube and returns them in an array\n
         results = the url results from youtube
         """
-        # Get the first 5 results
+        # Get the first MAX_RESULTS (default is 5) results
         videos = []
         results_used = []
         i = 0
         for result in results:
             try:
-                if (i == 5): break
+                if (i == MAX_RESULTS): break
 
                 if (result not in results_used):
                     yt = YouTube('https://www.youtube.com/watch?v=' + result)
 
                     minutes = yt.length / 60
-                    print(f'---------------------- =({i + 1})= ----------------------')
-                    print(f"Title: {yt.title}")
-                    print(f"Number of views: {yt.views}")
-                    print(f'Url: https://www.youtube.com/watch?v={result}')
-                    print(f"Length of video: {round(minutes, 2)} minutes ({yt.length} seconds)")
-                    print('---------------------------------------------------\n')
+                    log(f'---------------------- =({i + 1})= ----------------------')
+                    log(f"Title: {yt.title}")
+                    log(f"Number of views: {yt.views}")
+                    log(f'Url: https://www.youtube.com/watch?v={result}')
+                    log(f"Length of video: {round(minutes, 2)} minutes ({yt.length} seconds)")
+                    log('---------------------------------------------------\n')
 
                     results_used.append(result)
                     videos.append(yt)
@@ -209,20 +219,20 @@ class Downloader:
         # Ask user to choose a video
         while 1:
             choice = input('Enter the number of the video you want to download ("cancel" to cancel): ')
-            if choice == "none" or choice == "exit" or choice == "cancel" or choice == "q" or choice == "quit" or choice == "close":
+            if any(word in QUIT_CHARS for word in choice.split(" ")):
                 return None
 
             try:
                 pick = int(choice)
 
                 if (pick > len(videos) or pick <= 0):
-                    print('Invalid choice')
+                    log('Invalid choice')
                     continue
 
                 users_pick = pick
                 break
             except ValueError:
-                print('Invalid choice')
+                log('Invalid choice')
 
         return videos[users_pick - 1]
 
@@ -242,13 +252,13 @@ class Downloader:
             # Get results from the link as youtube title
             results = self.convert_to_url(self.link)
             if (results == None):
-                print('No results found')
+                log('No results found')
                 return
             else:
-                print(f'found {len(results)} results...')
+                log(f'found {len(results)} results...')
 
             # Get 5 of the most viewed videos
-            videos = self.print_results(results)
+            videos = self.log_results(results)
 
             # Get users input of which video they want to download
             yt = self.get_user_choice(videos)
@@ -259,12 +269,12 @@ class Downloader:
         now = datetime.now()
         currentTime = now.strftime("%d-%m-%Y")
 
-        # Download the video
-        self.download_video(yt, currentTime)
-        time.sleep(1) # Wait for the video to download, so there is no OSError
-        self.convert_to_sound(yt, currentTime)
+        # Download the video and return the file name to convert it to mp3 later
+        title = self.download_video(yt, currentTime)
+        time.sleep(0.5) # Wait for the video to download, so there is no OSError
+        succeeded = self.convert_to_sound(currentTime, title)
 
-        print("Download completed!")
+        log("Download completed!" if succeeded else "The download is incomplete, because of an error.")
         print('\n')
 
 def read_json_file():
@@ -272,15 +282,32 @@ def read_json_file():
     This function reads the json file and returns the data
     """
     try:
-        f = open('downloads/config.json', 'r')
+        f = open(DOWNLOAD_PATH + 'config.json', 'r')
         data = json.load(f)
         return data
-    except Exception as e:
-        print('Error: No downloads/config.json file found')
+    except Exception as ignored:
+        log(f'Error: No {DOWNLOAD_PATH}config.json file found')
         return None
 
+def log(message):
+    print(f"[YTDownloader] {message}")
+
 if __name__ == "__main__":
-    print('Started')
+    log(f"""
+    
+    _____.___._______  ____ ________                      .__                    .___            
+\__  |   |\__    ___/   \______ \   ______  _  ______ |  |   _________     __| _/___________ 
+ /   |   |  |    |       |    |  \ /  _ \ \/ \/ /    \|  |  /  _ \__  \   / __ |/ __ \_  __ 
+ \____   |  |    |       |    `   (  <_> )     /   |  \  |_(  <_> ) __ \_/ /_/ \  ___/|  | \/
+ / ______|  |____|      /_______  /\____/ \/\_/|___|  /____/\____(____  /\____ |\___  >__|   
+ \/                           \/                  \/                \/      \/    \/     
+    
+    Created by: {AUTHOR}
+    Quit keys: {QUIT_CHARS}
+    Download path: {DOWNLOAD_PATH}
+    Max results for video titles: {MAX_RESULTS}
+
+    """)
 
     dwnl = Downloader()
 
@@ -295,8 +322,8 @@ if __name__ == "__main__":
             client_id = "979507386832273449"
             RPC = Presence(client_id)
             RPC.connect()
-        except Exception as e:
-            print('Error: Could not connect to discord, please make sure you have discord running.')
+        except Exception as ignored:
+            log('Error: Could not connect to discord, please make sure you have discord running.')
 
     # Run the main function
     while 1:
@@ -304,11 +331,16 @@ if __name__ == "__main__":
             dwnl.set_presence('Stand by...')
 
         #ask for the link from user
-        link = input("Enter the link of YouTube video you want to download (\"cancel\" to quit): ")
-        if link == "none" or link == "exit" or link == "cancel":
+        link = input("Enter the title or link of the YouTube video you want to download (\"cancel\" to quit): ")
+        if any(word in QUIT_CHARS for word in link.split(" ")):
             sys.exit(1)
 
         if view_presence:
             dwnl.set_presence(f"Downloading {link}...")
         
-        dwnl.set_program(link)
+        #(?:v=|\/)([0-9A-Za-z_-]{11}).*
+        try:
+            dwnl.set_program(link)
+        except Exception as regex:
+            log('There seems to be an invalid character! A link may contain these characters:')
+            log(VALID_CHARS)
